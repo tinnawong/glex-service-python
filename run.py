1a2baef17978c2624e34efdef50d5ac3cb111d39
tinnawong <tinnawong2010@hotmail.com>
2020-09-01 17:17:16 +0700

    add command to build exe

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from demo import app
 from dotenv import load_dotenv
+import os
 load_dotenv('.env')
-# app.run(port=5200)
-# from gevent.pywsgi import WSGIServer
-# print("Start ")
-# http_server = WSGIServer(('', 5200), app)
-# http_server.serve_forever()
+os.system("flask run")
