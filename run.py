a71bddb5c120b43eef01c64e4c456b05dd9aa770
tinnawong <tinnawong2010@hotmail.com>
2020-09-03 20:35:37 +0700

    update

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 from demo import app
 from dotenv import load_dotenv
 import os
+from gevent.pywsgi import WSGIServer
 load_dotenv('.env')
-os.system("flask run")
+print("Start server :127.0.0.1:5200")
+http_server = WSGIServer(('127.0.0.1', 5200), app)
+http_server.serve_forever()
